a82acebe704068a667357883e745eb5f8cbef677
luis <luis_oliveira@usp.br>
2021-07-18 23:53:09 -0300

    Inserção de leitura de rotação (erro no lcd)

--- Pratica04.asm
+++ Pratica04.asm
@@ -1,111 +1,216 @@
 
 _timer_count:
 
-;Pratica04.c,17 :: 		void timer_count() iv 0x0008 ics ICS_AUTO {
-;Pratica04.c,19 :: 		}
+;Pratica04.c,20 :: 		void timer_count() iv 0x0008 ics ICS_AUTO {
+;Pratica04.c,21 :: 		rpm = TMR1H;
+	MOVF        TMR1H+0, 0 
+	MOVWF       _rpm+0 
+	MOVLW       0
+	MOVWF       _rpm+1 
+;Pratica04.c,22 :: 		rpm <<= 8;
+	MOVF        _rpm+0, 0 
+	MOVWF       _rpm+1 
+	CLRF        _rpm+0 
+;Pratica04.c,23 :: 		rpm += TMR1L;
+	MOVF        TMR1L+0, 0 
+	ADDWF       _rpm+0, 1 
+	MOVLW       0
+	ADDWFC      _rpm+1, 1 
+;Pratica04.c,25 :: 		TMR1H = 0x00;
+	CLRF        TMR1H+0 
+;Pratica04.c,26 :: 		TMR1L = 0x00;
+	CLRF        TMR1L+0 
+;Pratica04.c,28 :: 		TMR0H = 0x0B;
+	MOVLW       11
+	MOVWF       TMR0H+0 
+;Pratica04.c,29 :: 		TMR0L = 0xDC;
+	MOVLW       220
+	MOVWF       TMR0L+0 
+;Pratica04.c,30 :: 		}
 L_end_timer_count:
-L__timer_count8:
+L__timer_count9:
 	RETFIE      1
 ; end of _timer_count
 
 _read_button:
 
-;Pratica04.c,22 :: 		void read_button(){
-;Pratica04.c,23 :: 		if(PORTA.F0){
+;Pratica04.c,33 :: 		void read_button(){
+;Pratica04.c,34 :: 		if(PORTA.F0){
 	BTFSS       PORTA+0, 0 
 	GOTO        L_read_button0
-;Pratica04.c,24 :: 		CCPR1L = 0x00;
+;Pratica04.c,35 :: 		CCPR1L = 0x00;
 	CLRF        CCPR1L+0 
-;Pratica04.c,25 :: 		CCP1CON.F5 = 0;
+;Pratica04.c,36 :: 		CCP1CON.F5 = 0;
 	BCF         CCP1CON+0, 5 
-;Pratica04.c,26 :: 		CCP1CON.F4 = 0;
+;Pratica04.c,37 :: 		CCP1CON.F4 = 0;
 	BCF         CCP1CON+0, 4 
-;Pratica04.c,27 :: 		}
+;Pratica04.c,38 :: 		}
 L_read_button0:
-;Pratica04.c,28 :: 		if(PORTA.F1){
+;Pratica04.c,39 :: 		if(PORTA.F1){
 	BTFSS       PORTA+0, 1 
 	GOTO        L_read_button1
-;Pratica04.c,29 :: 		CCPR1L = 0x3F;
+;Pratica04.c,40 :: 		CCPR1L = 0x3F;
 	MOVLW       63
 	MOVWF       CCPR1L+0 
-;Pratica04.c,30 :: 		CCP1CON.F5 = 1;
+;Pratica04.c,41 :: 		CCP1CON.F5 = 1;
 	BSF         CCP1CON+0, 5 
-;Pratica04.c,31 :: 		CCP1CON.F4 = 1;
+;Pratica04.c,42 :: 		CCP1CON.F4 = 1;
 	BSF         CCP1CON+0, 4 
-;Pratica04.c,32 :: 		}
+;Pratica04.c,43 :: 		}
 L_read_button1:
-;Pratica04.c,33 :: 		if(PORTA.F2){
+;Pratica04.c,44 :: 		if(PORTA.F2){
 	BTFSS       PORTA+0, 2 
 	GOTO        L_read_button2
-;Pratica04.c,34 :: 		CCPR1L = 0x80;
+;Pratica04.c,45 :: 		CCPR1L = 0x80;
 	MOVLW       128
 	MOVWF       CCPR1L+0 
-;Pratica04.c,35 :: 		CCP1CON.F5 = 0;
+;Pratica04.c,46 :: 		CCP1CON.F5 = 0;
 	BCF         CCP1CON+0, 5 
-;Pratica04.c,36 :: 		CCP1CON.F4 = 0;
+;Pratica04.c,47 :: 		CCP1CON.F4 = 0;
 	BCF         CCP1CON+0, 4 
-;Pratica04.c,37 :: 		}
+;Pratica04.c,48 :: 		}
 L_read_button2:
-;Pratica04.c,38 :: 		if(PORTA.F3){
+;Pratica04.c,49 :: 		if(PORTA.F3){
 	BTFSS       PORTA+0, 3 
 	GOTO        L_read_button3
-;Pratica04.c,39 :: 		CCPR1L = 0xC0;
+;Pratica04.c,50 :: 		CCPR1L = 0xC0;
 	MOVLW       192
 	MOVWF       CCPR1L+0 
-;Pratica04.c,40 :: 		CCP1CON.F5 = 0;
+;Pratica04.c,51 :: 		CCP1CON.F5 = 0;
 	BCF         CCP1CON+0, 5 
-;Pratica04.c,41 :: 		CCP1CON.F4 = 0;
+;Pratica04.c,52 :: 		CCP1CON.F4 = 0;
 	BCF         CCP1CON+0, 4 
-;Pratica04.c,42 :: 		}
+;Pratica04.c,53 :: 		}
 L_read_button3:
-;Pratica04.c,43 :: 		if(PORTA.F4){
+;Pratica04.c,54 :: 		if(PORTA.F4){
 	BTFSS       PORTA+0, 4 
 	GOTO        L_read_button4
-;Pratica04.c,44 :: 		CCPR1L = 0xFF;
+;Pratica04.c,55 :: 		CCPR1L = 0xFF;
 	MOVLW       255
 	MOVWF       CCPR1L+0 
-;Pratica04.c,45 :: 		CCP1CON.F5 = 1;
+;Pratica04.c,56 :: 		CCP1CON.F5 = 1;
 	BSF         CCP1CON+0, 5 
-;Pratica04.c,46 :: 		CCP1CON.F4 = 1;
+;Pratica04.c,57 :: 		CCP1CON.F4 = 1;
 	BSF         CCP1CON+0, 4 
-;Pratica04.c,47 :: 		}
+;Pratica04.c,58 :: 		}
 L_read_button4:
-;Pratica04.c,48 :: 		}
+;Pratica04.c,59 :: 		}
 L_end_read_button:
 	RETURN      0
 ; end of _read_button
 
 _main:
 
-;Pratica04.c,50 :: 		void main(){
-;Pratica04.c,52 :: 		TRISC2_BIT = 0;
+;Pratica04.c,61 :: 		void main(){
+;Pratica04.c,63 :: 		Lcd_Init();
+	CALL        _Lcd_Init+0, 0
+;Pratica04.c,66 :: 		Lcd_Cmd(_LCD_CLEAR);
+	MOVLW       1
+	MOVWF       FARG_Lcd_Cmd_out_char+0 
+	CALL        _Lcd_Cmd+0, 0
+;Pratica04.c,69 :: 		Lcd_Cmd(_LCD_TURN_ON);
+	MOVLW       12
+	MOVWF       FARG_Lcd_Cmd_out_char+0 
+	CALL        _Lcd_Cmd+0, 0
+;Pratica04.c,72 :: 		TRISC2_BIT = 0;
 	BCF         TRISC2_bit+0, BitPos(TRISC2_bit+0) 
-;Pratica04.c,53 :: 		PR2 = 255;
+;Pratica04.c,75 :: 		TRISC0_BIT = 1;
+	BSF         TRISC0_bit+0, BitPos(TRISC0_bit+0) 
+;Pratica04.c,79 :: 		PR2 = 255;
 	MOVLW       255
 	MOVWF       PR2+0 
-;Pratica04.c,54 :: 		CCPR1L = 0b00000000;
+;Pratica04.c,83 :: 		CCPR1L = 0b00000000;
 	CLRF        CCPR1L+0 
-;Pratica04.c,55 :: 		CCP1CON = 0b00001100;
+;Pratica04.c,84 :: 		CCP1CON = 0b00001100;
 	MOVLW       12
 	MOVWF       CCP1CON+0 
-;Pratica04.c,60 :: 		T2CON = 0b00000001;
+;Pratica04.c,89 :: 		T2CON = 0b00000001;
 	MOVLW       1
 	MOVWF       T2CON+0 
-;Pratica04.c,63 :: 		TRISA = 0xFF;
+;Pratica04.c,92 :: 		TRISA = 0xFF;
 	MOVLW       255
 	MOVWF       TRISA+0 
-;Pratica04.c,66 :: 		ADCON1 = 0x0F;
+;Pratica04.c,95 :: 		ADCON1 = 0x0F;
 	MOVLW       15
 	MOVWF       ADCON1+0 
-;Pratica04.c,69 :: 		T2CON.F2 = 1;
+;Pratica04.c,98 :: 		T2CON.F2 = 1;
 	BSF         T2CON+0, 2 
-;Pratica04.c,74 :: 		while(1){
+;Pratica04.c,105 :: 		T1CON = 0b10000111;
+	MOVLW       135
+	MOVWF       T1CON+0 
+;Pratica04.c,112 :: 		T0CON = 0b00000100;
+	MOVLW       4
+	MOVWF       T0CON+0 
+;Pratica04.c,115 :: 		INTCON = 0b11100000;
+	MOVLW       224
+	MOVWF       INTCON+0 
+;Pratica04.c,118 :: 		INTCON2.F2 = 1;
+	BSF         INTCON2+0, 2 
+;Pratica04.c,121 :: 		T0CON.F0 = 1;
+	BSF         T0CON+0, 0 
+;Pratica04.c,123 :: 		TMR0H = 0x0B;
+	MOVLW       11
+	MOVWF       TMR0H+0 
+;Pratica04.c,124 :: 		TMR0L = 0xDC;
+	MOVLW       220
+	MOVWF       TMR0L+0 
+;Pratica04.c,126 :: 		while(1){
 L_main5:
-;Pratica04.c,75 :: 		read_button();
+;Pratica04.c,127 :: 		read_button();
 	CALL        _read_button+0, 0
-;Pratica04.c,76 :: 		}
+;Pratica04.c,129 :: 		Lcd_Cmd(_LCD_CLEAR);
+	MOVLW       1
+	MOVWF       FARG_Lcd_Cmd_out_char+0 
+	CALL        _Lcd_Cmd+0, 0
+;Pratica04.c,132 :: 		IntToStr(rpm, output);
+	MOVF        _rpm+0, 0 
+	MOVWF       FARG_IntToStr_input+0 
+	MOVF        _rpm+1, 0 
+	MOVWF       FARG_IntToStr_input+1 
+	MOVLW       _output+0
+	MOVWF       FARG_IntToStr_output+0 
+	MOVLW       hi_addr(_output+0)
+	MOVWF       FARG_IntToStr_output+1 
+	CALL        _IntToStr+0, 0
+;Pratica04.c,134 :: 		strcpy(output, "arroz\0");
+	MOVLW       _output+0
+	MOVWF       FARG_strcpy_to+0 
+	MOVLW       hi_addr(_output+0)
+	MOVWF       FARG_strcpy_to+1 
+	MOVLW       ?lstr1_Pratica04+0
+	MOVWF       FARG_strcpy_from+0 
+	MOVLW       hi_addr(?lstr1_Pratica04+0)
+	MOVWF       FARG_strcpy_from+1 
+	CALL        _strcpy+0, 0
+;Pratica04.c,137 :: 		Lcd_Out(1, 1, "teste");
+	MOVLW       1
+	MOVWF       FARG_Lcd_Out_row+0 
+	MOVLW       1
+	MOVWF       FARG_Lcd_Out_column+0 
+	MOVLW       ?lstr2_Pratica04+0
+	MOVWF       FARG_Lcd_Out_text+0 
+	MOVLW       hi_addr(?lstr2_Pratica04+0)
+	MOVWF       FARG_Lcd_Out_text+1 
+	CALL        _Lcd_Out+0, 0
+;Pratica04.c,139 :: 		Delay_ms(300);
+	MOVLW       4
+	MOVWF       R11, 0
+	MOVLW       12
+	MOVWF       R12, 0
+	MOVLW       51
+	MOVWF       R13, 0
+L_main7:
+	DECFSZ      R13, 1, 1
+	BRA         L_main7
+	DECFSZ      R12, 1, 1
+	BRA         L_main7
+	DECFSZ      R11, 1, 1
+	BRA         L_main7
+	NOP
+	NOP
+;Pratica04.c,140 :: 		}
 	GOTO        L_main5
-;Pratica04.c,77 :: 		}
+;Pratica04.c,141 :: 		}
 L_end_main:
 	GOTO        $+0
 ; end of _main
--- Pratica04.c
+++ Pratica04.c
@@ -14,8 +14,19 @@ sbit LCD_D6_Direction at TRISB2_bit;
 sbit LCD_D5_Direction at TRISB1_bit;
 sbit LCD_D4_Direction at TRISB0_bit;
 
-void timer_count() iv 0x0008 ics ICS_AUTO {
+unsigned int rpm=100;
+char output[15];
 
+void timer_count() iv 0x0008 ics ICS_AUTO {
+    rpm = TMR1H;
+    rpm <<= 8;
+    rpm += TMR1L;
+
+    TMR1H = 0x00;
+    TMR1L = 0x00;
+
+    TMR0H = 0x0B;
+    TMR0L = 0xDC;
 }
 
 //Leitura dos botoes
@@ -48,15 +59,31 @@ void read_button(){
 } 
 
 void main(){
+    // Inicia a utilizacao do LCD
+    Lcd_Init();
+
+    // Limpa o display
+    Lcd_Cmd(_LCD_CLEAR);
+
+    // Liga o display
+    Lcd_Cmd(_LCD_TURN_ON);
+    
     // Define o pino C2 como output
     TRISC2_BIT = 0;
 
+    // Define o pino C0 como input
+    TRISC0_BIT = 1;
+
     // Configuracao do PWM
+    // PR2 eh utilizado no calculo da frequencia do PWM
     PR2 = 255;
+
+    // Seta o valor inicial para um duty cicle 0
+    // Configura o modo CCP para PWM
     CCPR1L = 0b00000000;
     CCP1CON = 0b00001100;
 
-    // Configuracao do Timer2
+    // Configuracao do Timer2 - Sera usado para no PWM
     // Sem postscale
     // Prescaler de 4
     T2CON = 0b00000001;
@@ -70,10 +97,45 @@ void main(){
     // Ativa o Timer2
     T2CON.F2 = 1;
 
+    // Configuracao do Timer1 - Usado para contagem de pulsos do sensor infravermelho
+    // 16 bits
+    // Sem prescaler
+    // Utiliza pino RC0 como entrada de clock
+    // Ativa o contador
+    T1CON = 0b10000111;
 
+    // Configuracao do Timer0 - Usado para contar 1s
+    // 16 bits
+    // Utiliza clock do PIC
+    // Transicao low-to-high
+    // Prescaler 1:32
+    T0CON = 0b00000100;
 
+    // Habilita interrupcao do Timer0
+    INTCON = 0b11100000;
 
+    // Configura interrupcao do Timer0 como alta prioridade
+    INTCON2.F2 = 1;
+
+    // Inicia a contagem do timer
+    T0CON.F0 = 1;  
+
+    TMR0H = 0x0B;
+    TMR0L = 0xDC;
+    
     while(1){
         read_button();
+        // Limpa o display
+        Lcd_Cmd(_LCD_CLEAR);
+
+        // Realiza a conversao de float para cadeia de char para ser exibido no display
+        IntToStr(rpm, output);
+
+        strcpy(output, "arroz\0");
+
+        // Exibe o valor lido na primeira linha do display
+        Lcd_Out(1, 1, "teste");
+
+        Delay_ms(300);
     }
 }
